3d027abd2c6ab060acedbd42ea8817ecf413370a
Mark Pearl <markpearl@gmail.com>
2014-12-10 17:51:45 +0200

    Adapted UI to allow for single file lock

---
+++ WindowsFileLockerUtility/WindowsFileLockerUtility/Domain/FilesLocker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace WindowsFileLockerUtility.Domain
+{
+    public class FilesLocker
+    {
+        private readonly Dictionary<string, FileStream> _lockedFiles;
+
+        public FilesLocker()
+        {
+            _lockedFiles = new Dictionary<string, FileStream>();
+        }
+
+        public void LockFile(string filename)
+        {
+            var lockedFileStream = new FileStream(filename, FileMode.Open, FileAccess.ReadWrite, FileShare.None); 
+            _lockedFiles.Add(filename, lockedFileStream);
+        }
+
+        public void UnlockFile(string filename)
+        {
+            var fs = _lockedFiles[filename];
+            fs.Close();
+            _lockedFiles.Remove(filename);
+        }
+    }
+}
--- WindowsFileLockerUtility/WindowsFileLockerUtility/ViewModel/MainViewModel.cs
+++ WindowsFileLockerUtility/WindowsFileLockerUtility/ViewModel/MainViewModel.cs
@@ -1,28 +1,36 @@
+using System.IO;
+using System.Windows.Input;
+using WindowsFileLockerUtility.Domain;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
 
 namespace WindowsFileLockerUtility.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
     public class MainViewModel : ViewModelBase
     {
         private bool _locked;
+        private string _fileToLock;
+
+        private readonly FilesLocker _filesLocker;
+        private bool _isFileToLockEnabled;
+        private ICommand _addFile;
 
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
         public MainViewModel()
         {
+            _isFileToLockEnabled = true;
+            _filesLocker = new FilesLocker();
+        }
+
+        public ICommand AddFileCommand
+        {
+            get
+            {
+                return _addFile ?? (_addFile = new RelayCommand(SelectFileToAdd));
+            }
         }
 
         public bool Locked
@@ -35,6 +43,61 @@ namespace WindowsFileLockerUtility.ViewModel
             {
                 _locked = value;
                 RaisePropertyChanged(() => Locked);
+                UpdateLockedFile();
+            }
+        }
+
+        private void UpdateLockedFile()
+        {
+            if (!File.Exists(FileToLock))
+            {
+                Locked = false;
+            }
+
+            if (Locked)
+            {
+                _filesLocker.LockFile(FileToLock);
+                IsFileToLockEnabled = false;
+                return;
+            }
+
+            _filesLocker.UnlockFile(FileToLock);
+            IsFileToLockEnabled = true;
+        }
+
+        private void SelectFileToAdd()
+        {
+            var dialog = new OpenFileDialog();
+            dialog.InitialDirectory = "c:\\";
+            dialog.Filter = "All files (*.*)|*.*";
+            dialog.FilterIndex = 1;
+            dialog.RestoreDirectory = true;
+            var dialogResult = dialog.ShowDialog();
+            if (dialogResult != true) return;
+
+            FileToLock = dialog.FileName;
+        }
+
+        public string FileToLock
+        {
+            get
+            {
+                return _fileToLock;
+            }
+            set
+            {
+                _fileToLock = value;
+                RaisePropertyChanged(() => FileToLock);
+            }
+        }
+
+        public bool IsFileToLockEnabled
+        {
+            get { return _isFileToLockEnabled; }
+            set
+            {
+                _isFileToLockEnabled = value;
+                RaisePropertyChanged(() => IsFileToLockEnabled);
             }
         }
     }
